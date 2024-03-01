cc81018cf9127e5d56418cbeb8d4f8284d2a5e06
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-03-01 10:41:28 -0500

    feat(NewJokeRoute): added action for the form

--- jokes-app/app/routes/jokes.new.tsx
+++ jokes-app/app/routes/jokes.new.tsx
@@ -1,8 +1,27 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { Form, redirect } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+  const name = body.get("name");
+  const content = body.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  if (typeof content !== "string" || typeof name !== "string") {
+    throw new Error("Form not submitted correctly.");
+  }
+  const fields = { content, name };
+  const joke = await db.joke.create({
+    data: fields,
+  });
+  return redirect(`/jokes/${joke.id}`);
+}
+
 export default function NewJokeRoute() {
   return (
     <div>
       <p>Add your own hilarious joke</p>
-      <form method="post">
+      <Form method="post">
         <div>
           <label>
             Name: <input type="text" name="name" />
@@ -18,7 +37,7 @@ export default function NewJokeRoute() {
             Add
           </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
