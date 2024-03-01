7be1e7781496461cda1c9c4611f8feb2b81802de
Allison Truhlar <114596142+allison-truhlar@users.noreply.github.com>
2024-03-01 09:38:00 -0500

    feat(JokeRoute): display individual joke at jokes/:jokeId

--- jokes-app/app/routes/jokes.$jokeId.tsx
+++ jokes-app/app/routes/jokes.$jokeId.tsx
@@ -1,11 +1,24 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+  return json({ joke });
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke.content}</p>
     </div>
   );
 }
